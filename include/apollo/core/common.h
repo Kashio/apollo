e67940e500c4d45d65c3b42e5248908177536787
Roy Mor <roy.mor173@gmail.com>
2020-06-12 15:57:58 +0300

    change id_type typedef to std::size_t

--- include/apollo/core/common.h
+++ include/apollo/core/common.h
@@ -1,11 +1,11 @@
 #ifndef APOLLO_CORE_COMMON_H
 #define APOLLO_CORE_COMMON_H
 
-#include <cstdint>
+#include <cstddef>
 
 namespace apollo
 {
-	typedef uint64_t id_type;
+	typedef std::size_t id_type;
 	typedef id_type entity;
 }
 
